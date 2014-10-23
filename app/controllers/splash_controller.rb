9ce675d8ba399bea862f31882448a1ece874b437
bpwalford <bpwalford@gmail.com>
2014-10-23 10:43:58 -0600

    testing...

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,8 +1,16 @@
 class SplashController < ApplicationController
 
   def index
-    @user = request.location.country
-    @user3 = request.location.city
+
+    @userIp = request.remote_ip
+
+    @ipBreakDown = Geocoder.search("76.25.195.243")
+    @Gip = Geocoder.search("128.177.172.2")
+
+    @bar = Array.new
+
+    @var = request.env['HTTP_USER_AGENT']
+
   end
 
 end
