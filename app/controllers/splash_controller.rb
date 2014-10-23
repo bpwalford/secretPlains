1ab64ea39d945863dfaf191a5e429b8fe2a61039
bpwalford <bpwalford@gmail.com>
2014-10-22 18:43:16 -0600

    testing...

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -2,7 +2,6 @@ class SplashController < ApplicationController
 
   def index
     @user = request.location.country
-    @user2 = request.location.region_name
     @user3 = request.location.city
   end
 
