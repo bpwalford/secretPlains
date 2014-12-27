3a33cdd4bf6f0db382890098c1993952a67652e6
bpwalford <bpwalford@gmail.com>
2014-12-27 14:32:48 -0700

    still fixing issue...

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -2,18 +2,18 @@ class SplashController < ApplicationController
 
   def index
     @ip = request.remote_ip
-    userIp = Geocoder.search(@ip)[0].data
+    @userIp = Geocoder.search(@ip)[0].data
 
-    @ip           = userIp['ip']
-    @city         = userIp['city']
-    @region_code  = userIp['region_code']
-    @region_name  = userIp['region_name']
-    @metrocode    = userIp['metrocode']
-    @zipcode      = userIp['zipcode']
-    @latitude     = userIp['latitude']
-    @longitude    = userIp['longitude']
-    @country_name = userIp['country_name']
-    @country_code = userIp['country_code']
+    @ip           = @userIp['ip']
+    @city         = @userIp['city']
+    @region_code  = @userIp['region_code']
+    @region_name  = @userIp['region_name']
+    @metrocode    = @userIp['metrocode']
+    @zipcode      = @userIp['zipcode']
+    @latitude     = @userIp['latitude']
+    @longitude    = @userIp['longitude']
+    @country_name = @userIp['country_name']
+    @country_code = @userIp['country_code']
   end
 
   def test
