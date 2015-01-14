b1b7268076e5c9a66846cacea91e07cb26ef8fb8
bpwalford <bpwalford@gmail.com>
2015-01-14 06:43:43 -0700

    geocoder completely removed as dependency

--- Gemfile
+++ Gemfile
@@ -1,13 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'rails_12factor', group: :production
-gem 'geocoder'
 gem 'rails', '4.1.6'
 gem 'pg'
 gem 'sass-rails', '~> 4.0.3'
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.0.0'
 gem 'haml'
 gem 'faker'
 gem 'nokogiri'
--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,19 +1,6 @@
 class SplashController < ApplicationController
 
   def index
-    # ip = request.remote_ip
-    # userIp = Geocoder.search(ip)[0].data
-    #
-    # @ip           = userIp['ip']
-    # @city         = userIp['city']
-    # @region_code  = userIp['region_code']
-    # @region_name  = userIp['region_name']
-    # @metrocode    = userIp['metrocode']
-    # @zipcode      = userIp['zipcode']
-    # @latitude     = userIp['latitude']
-    # @longitude    = userIp['longitude']
-    # @country_name = userIp['country_name']
-    # @country_code = userIp['country_code']
   end
 
   def test
