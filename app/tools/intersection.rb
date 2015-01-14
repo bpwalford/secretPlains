b1110a77859df8aab818ea9026f66f9aa96ca0fb
bpwalford <bpwalford@gmail.com>
2015-01-14 13:05:48 -0700

    built class to calculate intersection of two sets

--- app/tools/intersection.rb
+++ app/tools/intersection.rb
@@ -1,20 +1,20 @@
 class Intersection
-
   attr_reader :set_one, :set_two
 
   def initialize(set_one, set_two)
-    @set_one = set_one
-    @set_two = set_two
+    # using sets because they have unique elements and order doesn't matter
+    @set_one = set_one.to_set
+    @set_two = set_two.to_set
   end
 
   def get_intersection
+    intersection = set_one + set_two
 
+    diff_one = set_one - set_two
+    diff_two = set_two - set_one
+    intersection -= diff_one
+    intersection -= diff_two
+
+    intersection
   end
-
-  def bar
-    2
-  end
-
-  private
-
 end
