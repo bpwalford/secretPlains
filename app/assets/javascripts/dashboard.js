c0a1f33bd2d421c325b3d3f4da39f6136bb80b79
bpwalford <bpwalford@gmail.com>
2015-01-21 20:24:59 -0700

    initial ui fingerprint finished (i.e. stucture done, need to present data)

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -10,16 +10,21 @@ $.get('/user_fingerprint')
     {type: 'ip', val: json.ip},
     {type: 'fonts', val: json.fonts},
     {type: 'language', val: json.language},
-    {type: 'screen', val: json.screen},
+    {type: 'screen size', val: json.screen},
     {type: 'browser version', val: json.browser_version},
     {type: 'user agent', val: json.user_agent},
   ]
 
-  var small  = 70;
-  var big    = 300;
-  var width  = '1110';
-  var height = '700';
-  var radius = '250';
+  var small  = 60;
+  var big    = 200;
+  var width  = '570';
+  var height = '570';
+  var radius = '210';
+  // var small  = 70;
+  // var big    = 300;
+  // var width  = '1110';
+  // var height = '700';
+  // var radius = '250';
 
 
   // calculate node placement
@@ -44,12 +49,7 @@ $.get('/user_fingerprint')
     return y;
   }
 
-
   // helper functions
-  function randomRgbNum() {
-    return Math.round(Math.random()*255).toString();
-  }
-
   function collapseNodes(){
     svg.selectAll('.small')
       .transition()
@@ -58,6 +58,10 @@ $.get('/user_fingerprint')
     svg.selectAll('text')
       .transition()
       .attr('font-size', '0')
+
+    svg.selectAll('line')
+      .transition()
+      .attr('opacity', '0')
   }
 
   function inflateNodes() {
@@ -70,49 +74,61 @@ $.get('/user_fingerprint')
       .transition()
       .delay(300)
       .attr('font-size', '15')
+
+    svg.selectAll('line')
+      .transition()
+      .delay(300)
+      .attr('opacity', '1')
   }
 
 
-  // fingerprint canvas
+  // canvas
   var svg = d3.select('#fingerprint')
     .append('svg')
     .attr('width', width)
     .attr('height', height);
 
 
-  // default circle attributes
+  // circle attributes
   var circles = svg.selectAll('circle')
     .data(data)
     .enter()
     .append('circle')
 
-  circles.attr('cx', function(d, i) {
-           return calculateX(d, i);
-         })
-         .attr('cy', function(d, i) {
-           return calculateY(d, i);
-         })
+  circles.attr('cx', function(d, i) { return calculateX(d, i); })
+         .attr('cy', function(d, i) { return calculateY(d, i); })
          .attr('r', small)
          .attr('class', 'small')
-         // .attr('fill', function() {
-         //   return  'rgb(' + randomRgbNum() + ', ' +
-         //                    randomRgbNum() + ', ' +
-         //                    randomRgbNum() + ')';
-         // })
          .attr('fill', 'rgb(103, 175, 233)')
-         .attr('stroke', 'silver')
-         .attr('stroke-width', '5')
+        //  .attr('stroke-width', '5')
 
+
+  // line attributes
+  var lines = svg.selectAll('line')
+    .data(data)
+    .enter()
+    .append('line');
+
+  lines.attr('stroke', 'rgb(103, 175, 233)')
+       .attr('stroke-width', '1')
+       .attr('x1', width/2)
+       .attr('y1', height/2)
+       .attr('x2', function(d) { return d.x; })
+       .attr('y2', function(d) { return d.y; })
+
+
+  // text attributes
   var labels = svg.selectAll('text')
     .data(data)
     .enter()
     .append('text')
-    .text(function(d) { return d.type })
+    .text(function(d) { return d.type });
 
   labels.attr('x', function(d) { return d.x - ((small/2) + 20)})
         .attr('y', function(d) { return d.y + 3 })
         .attr('font-size', '15')
-        .attr('fill', 'gray');
+        .attr('fill', 'silver');
+
 
   // circles on click alterations
   circles.on('click', function(d) {
@@ -120,17 +136,27 @@ $.get('/user_fingerprint')
       .attr('class', (this.classList[0] === 'big' ? 'small' : 'big'));
     if (this.classList[0] === 'big') {
       collapseNodes();
-      d3.select(this).transition()
-                     .delay(300)
-                     .attr('cx', width/2)
-                     .attr('cy', height/2)
-                     .attr('r', big);
+      d3.select(this)
+        .transition()
+        .delay(300)
+        .attr('cx', width/2)
+        .attr('cy', height/2)
+        .attr('r', big);
     } else {
       inflateNodes();
-      d3.select(this).transition()
-                     .attr('r', small)
-                     .attr('cx', function(d) { return d.x })
-                     .attr('cy', function(d) { return d.y });
+      d3.select(this)
+        .transition()
+        .attr('r', small)
+        .attr('cx', function(d) { return d.x })
+        .attr('cy', function(d) { return d.y });
     }
   })
 });
+
+
+
+
+// center bubble labels
+// draft bubble contents
+// make text clickable (maybe)
+// fill focus bubbles with content
