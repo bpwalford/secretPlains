3a648152142b11fc125fa84bba3395c367c0c31a
bpwalford <bpwalford@gmail.com>
2015-02-01 13:49:11 -0700

    reorganized js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 //= require jquery_ujs
 //= require d3
 //= require spinner.js
-//= require plugin_finder.js
-//= require font_list.js
-//= require font_detector.js
+//= require fingerprinting/plugin_finder.js
+//= require fingerprinting/font_list.js
+//= require fingerprinting/font_detector.js
 //= require_tree .
