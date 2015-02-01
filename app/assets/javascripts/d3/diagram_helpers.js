c166d310c8b619bb9cf663e32bc573ac1d1ba349
bpwalford <bpwalford@gmail.com>
2015-02-01 13:02:06 -0700

    reorganized d3 code

---
+++ app/assets/javascripts/d3/diagram_helpers.js
@@ -0,0 +1,55 @@
+function parsePlugins(plugins, elem) {
+  var $list = $(elem).append('<ul></ul>').find('ul');
+  for (var i = 0; i < plugins.length; i++) {
+    $list.append('<li>' + plugins[i] + '</li>');
+  }
+}
+
+function formatFonts(list) {
+  var formatted = '';
+
+  for (var i = 0; i < list.length; i++) {
+    formatted += list[i] + ', ';
+  }
+
+  return formatted;
+}
+
+function parseFonts(fontsObj, elem) {
+  var $list = $(elem).append('<ul></ul>').find('ul');
+
+  $list.append('<h4>' + 'installed fonts' + '</h4>');
+  var installed = formatFonts(fontsObj.installed);
+  $list.append('<li>' + installed +'</li>');
+
+  var uninstalled = formatFonts(fontsObj.uninstalled);
+  $list.append('<h4>' + 'uninstalled fonts' + '</h4>');
+  $list.append('<li>' + uninstalled + '</li>');
+}
+
+
+
+
+
+
+
+function calculateTheta(data, index) {
+  var scale = d3.scale.linear()
+  .domain([0, data.length])
+  .range([0, 2 * Math.PI]);
+  return scale(index);
+}
+
+function calculateX(obj, index, radius, data) {
+  var theta = calculateTheta(data, index);
+  var x = (width/2) + radius * Math.sin(theta);
+  obj.x = x;
+  return x;
+}
+
+function calculateY(obj, index, radius, data) {
+  var theta = calculateTheta(data, index);
+  var y = (height/2) + radius * Math.cos(theta);
+  obj.y = y;
+  return y;
+}
---
+++ app/assets/javascripts/d3/diagram_states.js
@@ -0,0 +1,50 @@
+'use strict';
+
+function collapseNodes(className, svg){
+  svg.selectAll('.small')
+    .transition()
+    .attr('r', '0');
+
+  svg.selectAll('text')
+    .transition()
+    .attr('font-size', '0');
+
+  svg.selectAll('line')
+    .transition()
+    .attr('opacity', '0');
+
+  svg.selectAll('.fingerprint-image')
+    .transition()
+    .attr('x', width - 344)
+    .attr('y', 280 - 39);
+
+  d3.selectAll('.' + className)
+    .transition()
+    .delay(300)
+    .style('display', 'block');
+}
+
+function inflateNodes(className, svg) {
+  svg.selectAll('.small')
+    .transition()
+    .delay(300)
+    .attr('r', small);
+
+  svg.selectAll('text')
+    .transition()
+    .delay(300)
+    .attr('font-size', font);
+
+  svg.selectAll('line')
+    .transition()
+    .delay(300)
+    .attr('opacity', '0.3');
+
+  svg.selectAll('.fingerprint-image')
+    .transition()
+    .attr('x', width/2 - 34)
+    .attr('y', height/2 - 43);
+
+  d3.selectAll('.' + className)
+    .style('display', 'none');
+}
