8819859b82df0a3b334d787d26cb74ad7a868c27
bpwalford <bpwalford@gmail.com>
2015-02-01 13:18:39 -0700

    restyled d3 diagram bubbles

--- app/assets/javascripts/d3/diagram_helpers.js
+++ app/assets/javascripts/d3/diagram_helpers.js
@@ -1,3 +1,29 @@
+// calculate circle x and y
+// ****************************************************************************
+function calculateTheta(data, index) {
+  var scale = d3.scale.linear()
+  .domain([0, data.length])
+  .range([0, 2 * Math.PI]);
+  return scale(index);
+}
+
+function calculateX(obj, index, radius, data) {
+  var theta = calculateTheta(data, index);
+  var x = (width/2) + radius * Math.sin(theta);
+  obj.x = x;
+  return x;
+}
+
+function calculateY(obj, index, radius, data) {
+  var theta = calculateTheta(data, index);
+  var y = (height/2) + radius * Math.cos(theta);
+  obj.y = y;
+  return y;
+}
+
+
+// build attribute text boxes
+// ****************************************************************************
 function parsePlugins(plugins, elem) {
   var $list = $(elem).append('<ul></ul>').find('ul');
   for (var i = 0; i < plugins.length; i++) {
@@ -25,31 +51,4 @@ function parseFonts(fontsObj, elem) {
   var uninstalled = formatFonts(fontsObj.uninstalled);
   $list.append('<h4>' + 'uninstalled fonts' + '</h4>');
   $list.append('<li>' + uninstalled + '</li>');
-}
-
-
-
-
-
-
-
-function calculateTheta(data, index) {
-  var scale = d3.scale.linear()
-  .domain([0, data.length])
-  .range([0, 2 * Math.PI]);
-  return scale(index);
-}
-
-function calculateX(obj, index, radius, data) {
-  var theta = calculateTheta(data, index);
-  var x = (width/2) + radius * Math.sin(theta);
-  obj.x = x;
-  return x;
-}
-
-function calculateY(obj, index, radius, data) {
-  var theta = calculateTheta(data, index);
-  var y = (height/2) + radius * Math.cos(theta);
-  obj.y = y;
-  return y;
 }
