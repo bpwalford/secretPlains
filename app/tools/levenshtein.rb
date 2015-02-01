85bb462497a6ea6debf851d51a9abe85910fff63
bpwalford <bpwalford@gmail.com>
2015-02-01 16:57:39 -0700

    found better version of levenshtein algorithm

--- app/tools/levenshtein.rb
+++ app/tools/levenshtein.rb
@@ -1,31 +1,19 @@
 class Levenshtein
-  attr_reader :string_one, :string_two
+  attr_reader :count
 
-  def initialize(string_one, string_two)
-    @string_one = string_one
-    @string_two = string_two
+  def initialize
+    @count = 0;
   end
 
-  def distance
-    matrix = [(0..string_one.length).to_a]
-    (1..string_two.length).each do |j|
-      matrix << [j] + [0] * (string_one.length)
-    end
+  def distance(str1, str2)
+    return str2.length if str1.empty?
+    return str1.length if str2.empty?
 
-    (1..string_two.length).each do |i|
-      (1..string_one.length).each do |j|
-        if string_one[j-1] == string_two[i-1]
-          matrix[i][j] = matrix[i-1][j-1]
-        else
-          matrix[i][j] = [
-            matrix[i-1][j],
-            matrix[i][j-1],
-            matrix[i-1][j-1],
-          ].min + 1
-        end
-      end
-    end
-
-    matrix.last.last
+    @count += 1
+    return distance(str1[1..-1], str2[1..-1]) if str1[0] == str2[0]
+    l1 = distance(str1, str2[1..-1])
+    l2 = distance(str1[1..-1], str2)
+    l3 = distance(str1[1..-1], str2[1..-1])
+    return 1 + [l1,l2,l3].min
   end
 end
