727d081c907eee444f48ad61e9a021eee1d4dc61
bpwalford <bpwalford@gmail.com>
2015-02-05 11:38:16 -0700

    changed diagram to partial that's rendered on login

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 root 'splash#index'
 
 get  'about'            => 'splash#about', as: :about
+
+get  'user_fingerprint' => 'get_fingerprint#retrieve', as: :user_fingerprint
+
 get  'dashboard'        => 'dashboards#index', as: :dashboard
 get  'fingerprint'      => 'dashboards#fingerprint', as: :fingerprint
-get  'user_fingerprint' => 'get_fingerprint#retrieve', as: :user_fingerprint
 post 'fingerprint'      => 'dashboards#record_fingerprint', as: :record_fingerprint
 
 get 'testprint883' => 'splash#test'
